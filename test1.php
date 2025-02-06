42abe1cb8a8e3a965da0e2a67f08af9fe4f01168
airlonger <airlonger.com@gmail.com>
2025-02-06 00:29:13 -0500

    added a test page

---
+++ test1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test one items</title>
+</head>
+<body>
+    <p>We have a thing to do, now and forever</p>
+</body>
+</html>
